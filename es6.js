29ec5811b1e148538f4c2abed0cb05603aa24340
Simon <szymon.dzialowski@gmail.com>
2017-11-07 10:22:43 +0000

    first commit

---
+++ es6.js
@@ -0,0 +1,14 @@
+
+
+const c = {
+    one: 'two',
+    test : 'eight'
+};
+
+const k = {
+    ...c,
+    three: 'four'
+}
+
+console.log(k)
+
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const path                  = require('path');
+
+module.exports = {
+    entry: {
+        index: path.resolve(__dirname, 'es6.js')
+    },
+    output: {
+        path: __dirname,
+        filename: "es5.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        babelrc: false,
+                        presets: [
+                            'babel-preset-env',
+                            'babel-preset-es2015',
+                            'babel-preset-stage-0'
+                        ],
+                        plugins: [
+                            'babel-plugin-transform-decorators-legacy',
+                        ]
+                    }
+                }
+            },
+        ]
+    }
+};
